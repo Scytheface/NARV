c685df5acb0d1dc8fb05a9485f927b5425c6228e
scytheface <tonis.hlebnikov@gmail.com>
2020-12-29 03:49:02 +0200

    featuring cut

--- scrapists/sasslantis.py
+++ scrapists/sasslantis.py
@@ -2,14 +2,17 @@ import bs4
 import requests
 
 
-def search(artist):
+def get_artist_lyrics(artist, featuring=False):
     resp = requests.get("https://sasslantis.ee/search.php", params={'q': artist})
     soup = bs4.BeautifulSoup(resp.text, "lxml")
     for result in soup.find('div', class_='col-md-12').find_all('div', class_='row'):
+        song_artist = result.find('h4').a.text.split('-')[0].strip().lower()
+        if not featuring and artist.lower() != song_artist:
+            continue
         lyrics_page = requests.get(f"https://sasslantis.ee/{result.find('a')['href']}")
         container = bs4.BeautifulSoup(lyrics_page.text, 'lxml').find('div', class_='col-md-8')
-        song_artist = container.find('a').next
-        song_title = song_artist.next
+
+        song_title = container.find('a').next.next
         song_lyrics = '\n'.join(s.strip() for s in container.find("h1").find_next_siblings(text=True))
 
         song_artist = song_artist.strip().lower()
@@ -18,4 +21,4 @@ def search(artist):
 
 
 if __name__ == '__main__':
-    print(*search('nublu'), sep='\n\n\n')
+    print(*get_artist_lyrics('nublu'), sep='\n\n\n')
